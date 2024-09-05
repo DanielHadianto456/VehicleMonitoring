89bbc5f20f397472f7711a31bcf37a1abfe05922
Daniel Hadianto <daniel88hadianto@gmail.com>
2024-09-05 20:33:10 +0700

    Feat: Added functions to retrieve order detail data

--- app/Http/Controllers/detailOrderController.php
+++ app/Http/Controllers/detailOrderController.php
@@ -4,8 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\ordersModel;
+use App\Models\detailOrderModel;
+use App\Models\driverModel;
+use App\Models\vehicleModel;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class detailOrderController extends Controller
 {
-    //
+    //Function used to get all details of any orders with it's foreign key values
+    public function getDetail(){
+        $detail = detailOrderModel::with('orderDetails')->get();
+        return response()->json($detail);
+    }
+
+    //Function used to get all details of an order based on id_detail_order
+    // with it's foreign key values
+    public function getDetailId($id){
+        $detail = detailOrderModel::with('orderDetails')->find($id); //input id_detail_order
+        return response()->json($detail);
+    }
+    
+    //Function used to get all details of an order based on id_order
+    // with it's foreign key values
+    public function getDetailIdOrder($id){
+        $detail = detailOrderModel::where('id_order', $id)::with('orderDetails')->first(); //input id_order
+        return response()->json($detail);
+    }
 }
--- app/Models/detailOrderModel.php
+++ app/Models/detailOrderModel.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class detailOrderModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'detail_order';
+
+    public $timestamps = false;
+
+    protected $primaryKey = 'id_detail_order';
+    
+    protected $fillable = [
+        'id_order',
+        'fuel_usage',
+        'used_at',
+        'returned_at',
+    ];
+
+    public function orderDetails(){
+        return $this->BelongsTo(ordersModel::class, 'id_order', 'id_order');
+    }
 }
